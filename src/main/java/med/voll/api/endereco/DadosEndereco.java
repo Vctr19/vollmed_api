460c9a77759dd7f83d55c86c09915d1a2e8e6fec
Victor <victor.rmsp@gmail.com>
2023-08-08 17:27:38 -0300

    cadastro de pacientes e exclusÃao de linhas em branco

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -4,19 +4,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record DadosEndereco(
-
         @NotBlank
         String logradouro,
-
         @NotBlank
         String bairro,
-
         @NotBlank @Pattern(regexp = "\\d{8}")
         String cep,
-
         @NotBlank
         String cidade,
-
         @NotBlank
         String uf,
         String complemento,
